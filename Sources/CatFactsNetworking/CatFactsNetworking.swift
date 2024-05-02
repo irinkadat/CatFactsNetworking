5254b388d4a76ecd0c4f895d773bcc8e5b3d778d
irinkadat <irina.datoshvili.1@btu.edu.ge>
2024-05-03 01:12:14 +0400

    Update CatFactViewController

--- Sources/CatFactsNetworking/CatFactsNetworking.swift
+++ Sources/CatFactsNetworking/CatFactsNetworking.swift
@@ -1,2 +1,33 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+public class CatFactsNetworking {
+    public init() {}
+    public static let shared = CatFactsNetworking()
+    public let baseURL = URL(string: "https://catfact.ninja/facts?max_length=150&limit=100")!
+    
+    public func fetchCatFacts(completion: @escaping ([CatFact]?, Error?) -> Void) {
+        let task = URLSession.shared.dataTask(with: baseURL) { data, response, error in
+            guard let data = data, error == nil else {
+                completion(nil, error)
+                return
+            }
+            
+            do {
+                let catFactResponse = try JSONDecoder().decode(CatFactResponse.self, from: data)
+                let catFacts = catFactResponse.data
+                completion(catFacts, nil)
+            } catch {
+                completion(nil, error)
+            }
+        }
+        task.resume()
+    }
+}
+
+public struct CatFactResponse: Decodable {
+    public let data: [CatFact]
+}
+
+public struct CatFact: Decodable {
+    public let fact: String
+}
